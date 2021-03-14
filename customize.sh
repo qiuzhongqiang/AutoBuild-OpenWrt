dc42de203b564ec5c41f48d2effeb2b335bfc4a9
QIUZHONGQIANG <qiuzhongqiang@users.noreply.github.com>
2021-03-14 16:39:10 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -8,4 +8,4 @@
 #1. Modify default IP
 sed -i 's/192.168.1.1/192.168.1.1/g' openwrt/package/base-files/files/bin/config_generate
 sed -i ' /uci commit system/i\uci set system. @system[0].hostname= '路由'' openwrt/ package/lean/default-settings/ files/zzz-default-settings
-sed -i " s/OpenWrt /qiu build $(TZ=UTC-8 date "+%Y .%m.%d") @ OpenWrt /g" openwrt/package/lean/ default-settings/files/zzz -default-settings
+sed -i " s/OpenWrt /qiang build $(TZ=UTC-8 date "+%Y .%m.%d") @ OpenWrt /g" openwrt/package/lean/ default-settings/files/zzz -default-settings
